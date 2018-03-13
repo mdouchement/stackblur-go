3c16ea432b1d2557f146ac302b768965a9aded66
mdouchement <marc.douchement@gmail.com>
2018-03-13 18:57:57 +0100

    Use default image.Image API and support any positive radius value
    - image.Image API is more readable
    - This implementation is slower than the original
      - 0.5s vs 2.4s for radius=20 and img size 6817x1283
      - 1.0s vs 2.2s for radius=20 and img size 3840x2160

--- stackblur.go
+++ stackblur.go
@@ -6,6 +6,7 @@ package stackblur
 import (
 	"image"
 	"image/color"
+	"image/draw"
 )
 
 // blurStack is a linked list containing the color value and a pointer to the next struct.
@@ -14,75 +15,40 @@ type blurStack struct {
 	next       *blurStack
 }
 
-var mulTable = []uint32{
-	512, 512, 456, 512, 328, 456, 335, 512, 405, 328, 271, 456, 388, 335, 292, 512,
-	454, 405, 364, 328, 298, 271, 496, 456, 420, 388, 360, 335, 312, 292, 273, 512,
-	482, 454, 428, 405, 383, 364, 345, 328, 312, 298, 284, 271, 259, 496, 475, 456,
-	437, 420, 404, 388, 374, 360, 347, 335, 323, 312, 302, 292, 282, 273, 265, 512,
-	497, 482, 468, 454, 441, 428, 417, 405, 394, 383, 373, 364, 354, 345, 337, 328,
-	320, 312, 305, 298, 291, 284, 278, 271, 265, 259, 507, 496, 485, 475, 465, 456,
-	446, 437, 428, 420, 412, 404, 396, 388, 381, 374, 367, 360, 354, 347, 341, 335,
-	329, 323, 318, 312, 307, 302, 297, 292, 287, 282, 278, 273, 269, 265, 261, 512,
-	505, 497, 489, 482, 475, 468, 461, 454, 447, 441, 435, 428, 422, 417, 411, 405,
-	399, 394, 389, 383, 378, 373, 368, 364, 359, 354, 350, 345, 341, 337, 332, 328,
-	324, 320, 316, 312, 309, 305, 301, 298, 294, 291, 287, 284, 281, 278, 274, 271,
-	268, 265, 262, 259, 257, 507, 501, 496, 491, 485, 480, 475, 470, 465, 460, 456,
-	451, 446, 442, 437, 433, 428, 424, 420, 416, 412, 408, 404, 400, 396, 392, 388,
-	385, 381, 377, 374, 370, 367, 363, 360, 357, 354, 350, 347, 344, 341, 338, 335,
-	332, 329, 326, 323, 320, 318, 315, 312, 310, 307, 304, 302, 299, 297, 294, 292,
-	289, 287, 285, 282, 280, 278, 275, 273, 271, 269, 267, 265, 263, 261, 259,
-}
-
-var shgTable = []uint32{
-	9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16, 17,
-	17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19,
-	19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20,
-	20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21,
-	21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21,
-	21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22,
-	22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-	22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23,
-	23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
-	23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
-	23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
-	23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
-	24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
-	24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
-	24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
-	24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
-}
-
 // NewBlurStack is a constructor function returning a new struct of type blurStack.
 func (bs *blurStack) NewBlurStack() *blurStack {
 	return &blurStack{bs.r, bs.g, bs.b, bs.a, bs.next}
 }
 
 // Process takes an image as parameter and returns it's blurred version by applying the blur radius.
-func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
+func Process(src image.Image, radius int, done chan struct{}) image.Image {
 	var stackEnd, stackIn, stackOut *blurStack
-	var width, height = uint32(src.Bounds().Dx()), uint32(src.Bounds().Dy())
+	var width, height = src.Bounds().Dx(), src.Bounds().Dy()
 	var (
-		div, widthMinus1, heightMinus1, radiusPlus1, sumFactor uint32
-		x, y, i, p, yp, yi, yw,
+		div, widthMinus1, heightMinus1, radiusPlus1, sumFactor, p int
 		rSum, gSum, bSum, aSum,
 		rOutSum, gOutSum, bOutSum, aOutSum,
 		rInSum, gInSum, bInSum, aInSum,
 		pr, pg, pb, pa uint32
 	)
 
-	img := toNRGBA(src)
+	// Copy image
+	img := image.NewNRGBA(src.Bounds())
+	draw.Draw(img, img.Bounds(), src, src.Bounds().Min, draw.Src)
 
 	div = radius + radius + 1
 	widthMinus1 = width - 1
 	heightMinus1 = height - 1
 	radiusPlus1 = radius + 1
 	sumFactor = radiusPlus1 * (radiusPlus1 + 1) / 2
+	divsum := uint32((div + 1) >> 1)
+	divsum *= divsum
 
 	bs := blurStack{}
 	stackStart := bs.NewBlurStack()
 	stack := stackStart
 
-	for i = 1; i < div; i++ {
+	for i := 1; i < div; i++ {
 		stack.next = bs.NewBlurStack()
 		stack = stack.next
 		if i == radiusPlus1 {
@@ -91,30 +57,28 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 	}
 	stack.next = stackStart
 
-	mulSum := mulTable[radius]
-	shgSum := shgTable[radius]
-
-	for y = 0; y < height; y++ {
+	for y := 0; y < height; y++ {
 		rInSum, gInSum, bInSum, aInSum, rSum, gSum, bSum, aSum = 0, 0, 0, 0, 0, 0, 0, 0
 
-		pr = uint32(img.Pix[yi])
-		pg = uint32(img.Pix[yi+1])
-		pb = uint32(img.Pix[yi+2])
-		pa = uint32(img.Pix[yi+3])
+		r, g, b, a := img.At(0, y).RGBA()
+		pr = to8b(r)
+		pg = to8b(g)
+		pb = to8b(b)
+		pa = to8b(a)
 
-		rOutSum = radiusPlus1 * pr
-		gOutSum = radiusPlus1 * pg
-		bOutSum = radiusPlus1 * pb
-		aOutSum = radiusPlus1 * pa
+		rOutSum = uint32(radiusPlus1) * pr
+		gOutSum = uint32(radiusPlus1) * pg
+		bOutSum = uint32(radiusPlus1) * pb
+		aOutSum = uint32(radiusPlus1) * pa
 
-		rSum += sumFactor * pr
-		gSum += sumFactor * pg
-		bSum += sumFactor * pb
-		aSum += sumFactor * pa
+		rSum += uint32(sumFactor) * pr
+		gSum += uint32(sumFactor) * pg
+		bSum += uint32(sumFactor) * pb
+		aSum += uint32(sumFactor) * pa
 
 		stack = stackStart
 
-		for i = 0; i < radiusPlus1; i++ {
+		for i := 0; i < radiusPlus1; i++ {
 			stack.r = pr
 			stack.g = pg
 			stack.b = pb
@@ -122,28 +86,27 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 			stack = stack.next
 		}
 
-		for i = 1; i < radiusPlus1; i++ {
-			var diff uint32
+		for i := 1; i < radiusPlus1; i++ {
+			p = i
 			if widthMinus1 < i {
-				diff = widthMinus1
-			} else {
-				diff = i
+				p = widthMinus1
 			}
-			p = yi + (diff << 2)
-			pr = uint32(img.Pix[p])
-			pg = uint32(img.Pix[p+1])
-			pb = uint32(img.Pix[p+2])
-			pa = uint32(img.Pix[p+3])
+
+			r, g, b, a := img.At(p, y).RGBA()
+			pr = to8b(r)
+			pg = to8b(g)
+			pb = to8b(b)
+			pa = to8b(a)
 
 			stack.r = pr
 			stack.g = pg
 			stack.b = pb
 			stack.a = pa
 
-			rSum += stack.r * (radiusPlus1 - i)
-			gSum += stack.g * (radiusPlus1 - i)
-			bSum += stack.b * (radiusPlus1 - i)
-			aSum += stack.a * (radiusPlus1 - i)
+			rSum += stack.r * uint32(radiusPlus1-i)
+			gSum += stack.g * uint32(radiusPlus1-i)
+			bSum += stack.b * uint32(radiusPlus1-i)
+			aSum += stack.a * uint32(radiusPlus1-i)
 
 			rInSum += pr
 			gInSum += pg
@@ -155,19 +118,13 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 		stackIn = stackStart
 		stackOut = stackEnd
 
-		for x = 0; x < width; x++ {
-			pa = (aSum * mulSum) >> shgSum
-			img.Pix[yi+3] = uint8(pa)
-
-			if pa != 0 {
-				img.Pix[yi] = uint8((rSum * mulSum) >> shgSum)
-				img.Pix[yi+1] = uint8((gSum * mulSum) >> shgSum)
-				img.Pix[yi+2] = uint8((bSum * mulSum) >> shgSum)
-			} else {
-				img.Pix[yi] = 0
-				img.Pix[yi+1] = 0
-				img.Pix[yi+2] = 0
-			}
+		for x := 0; x < width; x++ {
+			img.Set(x, y, color.NRGBA{
+				R: uint8(rSum / divsum),
+				G: uint8(gSum / divsum),
+				B: uint8(bSum / divsum),
+				A: uint8(aSum / divsum),
+			})
 
 			rSum -= rOutSum
 			gSum -= gOutSum
@@ -184,12 +141,12 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 			if p > widthMinus1 {
 				p = widthMinus1
 			}
-			p = (yw + p) << 2
 
-			stackIn.r = uint32(img.Pix[p])
-			stackIn.g = uint32(img.Pix[p+1])
-			stackIn.b = uint32(img.Pix[p+2])
-			stackIn.a = uint32(img.Pix[p+3])
+			r, g, b, a := img.At(p, y).RGBA()
+			stackIn.r = to8b(r)
+			stackIn.g = to8b(g)
+			stackIn.b = to8b(b)
+			stackIn.a = to8b(a)
 
 			rInSum += stackIn.r
 			gInSum += stackIn.g
@@ -219,34 +176,31 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 			aInSum -= pa
 
 			stackOut = stackOut.next
-
-			yi += 4
 		}
-		yw += width
 	}
 
-	for x = 0; x < width; x++ {
+	for x := 0; x < width; x++ {
 		rInSum, gInSum, bInSum, aInSum, rSum, gSum, bSum, aSum = 0, 0, 0, 0, 0, 0, 0, 0
 
-		yi = x << 2
-		pr = uint32(img.Pix[yi])
-		pg = uint32(img.Pix[yi+1])
-		pb = uint32(img.Pix[yi+2])
-		pa = uint32(img.Pix[yi+3])
+		r, g, b, a := img.At(x, 0).RGBA()
+		pr = to8b(r)
+		pg = to8b(g)
+		pb = to8b(b)
+		pa = to8b(a)
 
-		rOutSum = radiusPlus1 * pr
-		gOutSum = radiusPlus1 * pg
-		bOutSum = radiusPlus1 * pb
-		aOutSum = radiusPlus1 * pa
+		rOutSum = uint32(radiusPlus1) * pr
+		gOutSum = uint32(radiusPlus1) * pg
+		bOutSum = uint32(radiusPlus1) * pb
+		aOutSum = uint32(radiusPlus1) * pa
 
-		rSum += sumFactor * pr
-		gSum += sumFactor * pg
-		bSum += sumFactor * pb
-		aSum += sumFactor * pa
+		rSum += uint32(sumFactor) * pr
+		gSum += uint32(sumFactor) * pg
+		bSum += uint32(sumFactor) * pb
+		aSum += uint32(sumFactor) * pa
 
 		stack = stackStart
 
-		for i = 0; i < radiusPlus1; i++ {
+		for i := 0; i < radiusPlus1; i++ {
 			stack.r = pr
 			stack.g = pg
 			stack.b = pb
@@ -254,24 +208,22 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 			stack = stack.next
 		}
 
-		yp = width
-
-		for i = 1; i <= radius; i++ {
-			yi = (yp + x) << 2
-			pr = uint32(img.Pix[yi])
-			pg = uint32(img.Pix[yi+1])
-			pb = uint32(img.Pix[yi+2])
-			pa = uint32(img.Pix[yi+3])
+		for i := 1; i <= radius; i++ {
+			r, g, b, a := img.At(x, i).RGBA()
+			pr = to8b(r)
+			pg = to8b(g)
+			pb = to8b(b)
+			pa = to8b(a)
 
 			stack.r = pr
 			stack.g = pg
 			stack.b = pb
 			stack.a = pa
 
-			rSum += stack.r * (radiusPlus1 - i)
-			gSum += stack.g * (radiusPlus1 - i)
-			bSum += stack.b * (radiusPlus1 - i)
-			aSum += stack.a * (radiusPlus1 - i)
+			rSum += stack.r * uint32(radiusPlus1-i)
+			gSum += stack.g * uint32(radiusPlus1-i)
+			bSum += stack.b * uint32(radiusPlus1-i)
+			aSum += stack.a * uint32(radiusPlus1-i)
 
 			rInSum += pr
 			gInSum += pg
@@ -279,30 +231,18 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 			aInSum += pa
 
 			stack = stack.next
-
-			if i < heightMinus1 {
-				yp += width
-			}
 		}
 
-		yi = x
 		stackIn = stackStart
 		stackOut = stackEnd
 
-		for y = 0; y < height; y++ {
-			p = yi << 2
-			pa = (aSum * mulSum) >> shgSum
-			img.Pix[p+3] = uint8(pa)
-
-			if pa > 0 {
-				img.Pix[p] = uint8((rSum * mulSum) >> shgSum)
-				img.Pix[p+1] = uint8((gSum * mulSum) >> shgSum)
-				img.Pix[p+2] = uint8((bSum * mulSum) >> shgSum)
-			} else {
-				img.Pix[p] = 0
-				img.Pix[p+1] = 0
-				img.Pix[p+2] = 0
-			}
+		for y := 0; y < height; y++ {
+			img.Set(x, y, color.NRGBA{
+				R: uint8(rSum / divsum),
+				G: uint8(gSum / divsum),
+				B: uint8(bSum / divsum),
+				A: uint8(aSum / divsum),
+			})
 
 			rSum -= rOutSum
 			gSum -= gOutSum
@@ -319,12 +259,11 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 			if p > heightMinus1 {
 				p = heightMinus1
 			}
-			p = (x + (p * width)) << 2
-
-			stackIn.r = uint32(img.Pix[p])
-			stackIn.g = uint32(img.Pix[p+1])
-			stackIn.b = uint32(img.Pix[p+2])
-			stackIn.a = uint32(img.Pix[p+3])
+			r, g, b, a := img.At(x, p).RGBA()
+			stackIn.r = to8b(r)
+			stackIn.g = to8b(g)
+			stackIn.b = to8b(b)
+			stackIn.a = to8b(a)
 
 			rInSum += stackIn.r
 			gInSum += stackIn.g
@@ -354,83 +293,12 @@ func Process(src image.Image, radius uint32, done chan struct{}) image.Image {
 			aInSum -= pa
 
 			stackOut = stackOut.next
-
-			yi += width
 		}
 	}
 	done <- struct{}{}
 	return img
 }
 
-// toNRGBA converts any image type to *image.NRGBA with min-point at (0, 0).
-func toNRGBA(img image.Image) *image.NRGBA {
-	srcBounds := img.Bounds()
-	if srcBounds.Min.X == 0 && srcBounds.Min.Y == 0 {
-		if src0, ok := img.(*image.NRGBA); ok {
-			return src0
-		}
-	}
-	srcMinX := srcBounds.Min.X
-	srcMinY := srcBounds.Min.Y
-
-	dstBounds := srcBounds.Sub(srcBounds.Min)
-	dstW := dstBounds.Dx()
-	dstH := dstBounds.Dy()
-	dst := image.NewNRGBA(dstBounds)
-
-	switch src := img.(type) {
-	case *image.NRGBA:
-		rowSize := srcBounds.Dx() * 4
-		for dstY := 0; dstY < dstH; dstY++ {
-			di := dst.PixOffset(0, dstY)
-			si := src.PixOffset(srcMinX, srcMinY+dstY)
-			for dstX := 0; dstX < dstW; dstX++ {
-				copy(dst.Pix[di:di+rowSize], src.Pix[si:si+rowSize])
-			}
-		}
-	case *image.YCbCr:
-		for dstY := 0; dstY < dstH; dstY++ {
-			di := dst.PixOffset(0, dstY)
-			for dstX := 0; dstX < dstW; dstX++ {
-				srcX := srcMinX + dstX
-				srcY := srcMinY + dstY
-				siy := src.YOffset(srcX, srcY)
-				sic := src.COffset(srcX, srcY)
-				r, g, b := color.YCbCrToRGB(src.Y[siy], src.Cb[sic], src.Cr[sic])
-				dst.Pix[di+0] = r
-				dst.Pix[di+1] = g
-				dst.Pix[di+2] = b
-				dst.Pix[di+3] = 0xff
-				di += 4
-			}
-		}
-	case *image.Gray:
-		for dstY := 0; dstY < dstH; dstY++ {
-			di := dst.PixOffset(0, dstY)
-			si := src.PixOffset(srcMinX, srcMinY+dstY)
-			for dstX := 0; dstX < dstW; dstX++ {
-				c := src.Pix[si]
-				dst.Pix[di+0] = c
-				dst.Pix[di+1] = c
-				dst.Pix[di+2] = c
-				dst.Pix[di+3] = 0xff
-				di += 4
-				si += 2
-			}
-		}
-	default:
-		for dstY := 0; dstY < dstH; dstY++ {
-			di := dst.PixOffset(0, dstY)
-			for dstX := 0; dstX < dstW; dstX++ {
-				c := color.NRGBAModel.Convert(img.At(srcMinX+dstX, srcMinY+dstY)).(color.NRGBA)
-				dst.Pix[di+0] = c.R
-				dst.Pix[di+1] = c.G
-				dst.Pix[di+2] = c.B
-				dst.Pix[di+3] = c.A
-				di += 4
-			}
-		}
-	}
-
-	return dst
+func to8b(c uint32) uint32 {
+	return uint32(uint8(c)) // Go on 0..255 values
 }
